1f113e260e44f5b31521d1225142cb83042a76bf
Ayobami <quadrijamiu2014@gmail.com>
2023-02-23 22:40:35 +0100

    finished basic function overloading"

--- Day2/parseCoordinate.ts
+++ Day2/parseCoordinate.ts
@@ -1,30 +1,29 @@
+// /////////////
+// Basic of function overloading
+// ///////////////////////
+
 interface Coordinate {
     x: number,
     y: number
 }
 
-function parseCoordinateFromObject(obj: Coordinate):Coordinate{
-    return{
-        ...obj,
-    }
-}
-
-function parseCoordinateFromNumbers(x: number, y:number):Coordinate{
-    return{
-        x,
-        y
-    }
-}
 
 function parseCoordinate(obj: Coordinate):Coordinate;
 function parseCoordinate(x: number, y: number):Coordinate;
-function parseCoordinate(arg1: unknown, arg2: unknown):Coordinate{
+function parseCoordinate(arg1: unknown, arg2?: unknown):Coordinate{
     let coord: Coordinate={
         x:0,
         y:0
     };
 
-    if (typeof arg1 === "object"){
+    if (typeof arg1 === "string"){
+        (arg1 as string).split(',').forEach(str =>{
+            const [key,value] = str.split(":");
+            coord[key as "x" | "y"] = parseInt(value,10)
+        });
+    
+    }
+    else if (typeof arg1 === "object"){
         coord = {
             ...(arg1 as Coordinate),
         };
